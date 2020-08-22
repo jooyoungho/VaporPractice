c433ac1fee03031950aa08a6d2a140cbac807d67
Youngho <creative@yonsei.ac.kr>
2020-08-23 05:16:48 +0900

    Add: server configuration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,7 +4,11 @@ import Vapor
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+    
+    //server configuration
+    app.http.server.configuration.hostname = "0.0.0.0"
+    app.http.server.configuration.port = 8080
+    
     // register routes
     try routes(app)
 }
