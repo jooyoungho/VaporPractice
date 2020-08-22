dc0e6f36a68df41555096ca44f2c0bac7507947a
Youngho <creative@yonsei.ac.kr>
2020-08-23 02:40:29 +0900

    Add: Route Parameters

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,8 @@ func routes(_ app: Application) throws {
     app.get { req in
         return "It works!"
     }
-
+    
+    //Route Parameters
     app.get("hello", ":name") { req -> String in
         let name = req.parameters.get("name")!
         return "Hello, \(name)"
