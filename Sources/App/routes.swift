d1d5025884153e443b25e9c32d5b8bb5b7c65c03
Youngho <creative@yonsei.ac.kr>
2020-08-23 02:25:49 +0900

    init

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,7 +5,9 @@ func routes(_ app: Application) throws {
         return "It works!"
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    app.get("hello", ":name") { req -> String in
+        let name = req.parameters.get("name")!
+        return "Hello, \(name)"
     }
+    
 }
